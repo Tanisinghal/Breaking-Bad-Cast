1dd81dbc46e4a153a88b287f4fa0ac7b14fc6a60
Tanishka Singhal <77460050+Tanisinghal@users.noreply.github.com>
2021-08-11 10:32:02 +0530

    Update CharacterItem.js

--- src/component/CharacterItem.js
+++ src/component/CharacterItem.js
@@ -37,7 +37,7 @@ const CharacterItem = ({ item }) => {
             <li>
               <strong>Status:</strong> {item.status}
             </li>
-            <a href="!#">Read More</a>
+            
           </ul>
         </div>
       </div>
